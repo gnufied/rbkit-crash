bf2374cdc2333a27368821d30b2f72da16d33ab3
Hemant Kumar <gethemant@gmail.com>
2014-09-27 05:52:59 -0600

    ad code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RbkitTest::Application.config.secret_key_base = '9a8f049a1a94f1c9e363029e37980df936d0ab29079a7888b0ec5e2998d01f4ba07eb70173dbd0df6ae903b01fb431f0c4ebbfc42ca167d3d88bc957402f82ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RbkitTest::Application.config.session_store :cookie_store, key: '_rbkit_test_session'
